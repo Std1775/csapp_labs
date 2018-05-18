d8979fc33773778861c133b2b25524e22271bb5d
Std1775 <xinyuewang.work@gmail.com>
2018-05-17 18:09:48 -0700

    Datalab Question#3

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -165,7 +165,8 @@ int getByte(int x, int n) {
  *   Rating: 3 
  */
 int logicalShift(int x, int n) {
-  return 2;
+  int mask = 0xFFFF;
+  return x & (~ (mask) << (0x100 + (~n)));
 }
 /*
  * bitCount - returns count of number of 1's in word
